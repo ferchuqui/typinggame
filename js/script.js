257d6063cc8a3cd1e101b9e302efe4d3125d0aa3
ferchuqui <luisfernandofarji@hotmail.com>
2019-12-31 16:59:55 -0300

    dando forma al area de texto

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
 
 function getRandomQuote() {
  return fetch(RANDOM_QUOTE_API_URL)
@@ -9,5 +10,11 @@ function getRandomQuote() {
 async function renderNewQuote(){
  const quote = await getRandomQuote()
 quoteDisplayElement.innerText = quote
+quote.split('').forEach(character =>{
+  const characterSpan = document.createElement('span')
+characterSpan.innerText =character
+  quoteDisplayElement.appendChild(characterSpan)
+})
+quoteInputElement.value = null
 }
 renderNewQuote()
