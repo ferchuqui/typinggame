16dd138e0242b2a48d6c3e48718d3f1bd2447f9a
ferchuqui <luisfernandofarji@hotmail.com>
2019-12-31 18:49:52 -0300

    installing timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+let correct = true
 
+quoteInputElement.addEventListener('input', ()=>{
+  const arrayQuote =quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null){
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if(character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else{
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
+  if (correct) renderNewQuote()
+})
 function getRandomQuote() {
  return fetch(RANDOM_QUOTE_API_URL)
   .then(response => response.json())
@@ -9,12 +30,26 @@ function getRandomQuote() {
 }
 async function renderNewQuote(){
  const quote = await getRandomQuote()
-quoteDisplayElement.innerText = quote
+quoteDisplayElement.innerHTML = ''
 quote.split('').forEach(character =>{
   const characterSpan = document.createElement('span')
 characterSpan.innerText =character
   quoteDisplayElement.appendChild(characterSpan)
 })
 quoteInputElement.value = null
+startTimer()
+}
+
+let starTime
+function startTimer() {
+  timerElement.innerText = 0
+  starTime = new Date()
+  setInterval(()=>{
+    timer.innerText = getTimerTime()
+  },1000)
+}
+
+function getTimerTime(){
+ return Math.floor ((new Date() - starTime) / 1000)
 }
 renderNewQuote()
